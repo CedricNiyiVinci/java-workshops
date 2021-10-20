794aa5cf0cc591072ae8fb69e1f41e1cbf7fce01
Cedric Niyikiza <cedric.niyikiza@student.vinci.be>
2021-10-20 09:32:53 +0200

    Initialisation folders

---
+++ workshop06/src/domaine/PrixTest.java
@@ -0,0 +1,33 @@
+package domaine;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrixTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getTypePromo() {
+    }
+
+    @Test
+    void getValeurPromo() {
+    }
+
+    @Test
+    void definirPrix() {
+    }
+
+    @Test
+    void getPrix() {
+    }
+
+    @Test
+    void testClone() {
+    }
+}
