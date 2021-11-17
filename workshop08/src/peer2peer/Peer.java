7ce304ed43a3b50d10c70e60aff59de4089e9678
20201652 <cedric.niyikiza@student.vinci.be>
2021-11-17 11:47:41 +0100

    fin de l'exo 2 — P2P Deadlock

--- workshop08/src/peer2peer/Peer.java
+++ workshop08/src/peer2peer/Peer.java
@@ -13,13 +13,19 @@ public class Peer {
     }
 
     //TODO: modifier cette méthode pour éviter le deadlock.
-    public synchronized void connecter(Peer peer) {
-        if(connexions.contains(peer)) {
-            System.out.println(adresse + " est déjà connecté à " + peer.adresse);
-            return;
+    public void connecter(Peer peer) {
+
+        synchronized (this) {
+            if (connexions.contains(peer)) {
+                System.out.println(adresse + " est déjà connecté à " + peer.adresse);
+                return;
+            }
+
+            connexions.add(peer);
+            System.out.println(adresse + " est maintenant connecté à " + peer.adresse);
         }
-        connexions.add(peer);
-        System.out.println(adresse + " est maintenant connecté à " + peer.adresse);
-        peer.connecter(this);
+            peer.connecter(this);
+
+
     }
 }
