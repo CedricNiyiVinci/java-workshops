6d4332634f7216fd504cdfe9ef8e99aed1ed4820
20201652 <cedric.niyikiza@student.vinci.be>
2021-11-24 12:46:56 +0100

    Macro code — Etape 3

---
+++ workshop09-server/src/main/java/ServerThread.java
@@ -0,0 +1,11 @@
+public class ServerThread extends Thread{
+    //attribut socket
+
+
+    //constructeur avec parametre socket
+        // appelle constructeur Thread
+        //initialise att socket
+
+    //fonction Run
+
+}
